1b885db115bfbdf6c205b1cf972084a1e8cbe6f0
Cade Daniel <edacih@gmail.com>
2022-09-09 22:50:25 +0000

    renaming tf script

--- air-benchmark-wip/preprocess.py
+++ air-benchmark-wip/preprocess.py
@@ -5,6 +5,7 @@ import numpy as np
 dataset_size = 100
 image_dims = (1, 224, 224, 3)
 
+# TODO need to download mnist and perform resnet50 preprocessing.
 dataset = np.zeros((dataset_size, *image_dims, 1), dtype=np.int32)
 
 np.save('preprocessed_data.npy', dataset)
