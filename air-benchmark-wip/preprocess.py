076f635aec4294bcd8add669112b105fc4828190
Cade Daniel <edacih@gmail.com>
2022-09-09 22:44:06 +0000

    Removing unnecessary code

--- air-benchmark-wip/preprocess.py
+++ air-benchmark-wip/preprocess.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3.9
 
 import numpy as np
-import time
 
 dataset_size = 100
 image_dims = (1, 224, 224, 3)
 
 dataset = np.zeros((dataset_size, *image_dims, 1), dtype=np.int32)
-#labels = np.zeros((dataset_size, 1), dtype=np.int32)
 
 np.save('preprocessed_data.npy', dataset)
