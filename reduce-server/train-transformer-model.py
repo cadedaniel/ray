8d569066b0d253d2992b80d7dfa3bde6a68ffb4b
Cade Daniel <edacih@gmail.com>
2023-03-03 15:40:24 -0800

    DDP using notebook_launcher

--- reduce-server/train-transformer-model.py
+++ reduce-server/train-transformer-model.py
@@ -1,91 +1,103 @@
 #!/usr/bin/env python3
 
-import subprocess
-subprocess.run("pip install -U 'numpy<1.24.0' accelerate transformers 'dill<0.3.5' datasets", shell=True)
-
 import os
 os.environ['WANDB_DISABLED'] = 'true'
 os.environ['COMET_MODE'] = 'disabled'
-os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 
-print('import torch')
-import torch
+#local_rank = os.environ.get('LOCAL_RANK', -1)
+#os.environ['CUDA_VISIBLE_DEVICES'] = str(local_rank)
 
-print('import transformers')
-from transformers import BloomTokenizerFast, BloomForCausalLM
-from transformers import Trainer, TrainingArguments
+#if local_rank == 0:
+#    # TODO this will have problems, other ranks don't wait
+#    # need barrier
+#    import subprocess
+#    subprocess.run("pip install -U 'numpy<1.24.0' accelerate transformers 'dill<0.3.5' datasets", shell=True)
 
-print('init tokenizer')
-tokenizer = BloomTokenizerFast.from_pretrained("bigscience/bloom-560m")
+def train_single_proc():
 
-print('init model')
-model = BloomForCausalLM.from_pretrained(
-    "bigscience/bloom-560m",
-    #torch_dtype=torch.float16, -> Attempting to unscale FP16 gradients.
-)
+    print('import torch')
+    import torch
+    
+    print('import transformers')
+    from transformers import BloomTokenizerFast, BloomForCausalLM
+    from transformers import Trainer, TrainingArguments
+    
+    print('init tokenizer')
+    tokenizer = BloomTokenizerFast.from_pretrained("bigscience/bloom-560m")
+    
+    print('init model')
+    model = BloomForCausalLM.from_pretrained(
+        "bigscience/bloom-560m",
+        #torch_dtype=torch.float16, -> Attempting to unscale FP16 gradients.
+        #device_map="sequential",
+    )
+    
+    #model = model.to(torch.float16) -> Attempting to unscale FP16 gradients.
+    
+    print('load dataset')
+    from datasets import load_dataset
+    dataset = load_dataset("pile-of-law/pile-of-law",'r_legaladvice')
+    
+    def tokenize_function(examples):
+        return tokenizer(examples["text"])
+    
+    import os
+    
+    print('preprocess dataset')
+    tokenized_dataset = dataset.map(tokenize_function, 
+                                    batched=True, 
+                                    num_proc=os.cpu_count(),
+                                    remove_columns=["text","created_timestamp","downloaded_timestamp","url"],
+    )
+    
+    block_size = 128
+    def group_texts(examples):
+        # Concatenate all texts.
+        concatenated_examples = {k: sum(examples[k], []) for k in examples.keys()}
+        total_length = len(concatenated_examples[list(examples.keys())[0]])
+        # We drop the small remainder, we could add padding if the model supported it instead of this drop, you can
+            # customize this part to your needs.
+        total_length = (total_length // block_size) * block_size
+        # Split by chunks of max_len.
+        result = {
+     k: [t[i : i + block_size] for i in range(0, total_length, block_size)]
+            for k, t in concatenated_examples.items()
+        }
+        result["labels"] = result["input_ids"].copy()
+        return result
+    
+    lm_datasets = tokenized_dataset.map(
+        group_texts,
+        batched=True,
+        batch_size=1000,
+        num_proc=os.cpu_count(),
+    )
+    
+    print('creating trainer')
+    training_args = TrainingArguments(
+        f"bloom560m-finetuned-pileoflaw_reddit",
+        per_device_train_batch_size=16,
+        #gradient_checkpointing=True,
+        gradient_accumulation_steps=1,
+        optim="adafactor",
+        logging_steps=40,
+        save_strategy='epoch',
+        weight_decay=0.1,
+        learning_rate=5e-6,
+        evaluation_strategy='steps',
+        eval_steps=400,
+        per_device_eval_batch_size=16,
+    )
+    
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=lm_datasets["train"],
+        eval_dataset=lm_datasets["validation"],
+    )
+    
+    trainer.train()
 
-#model = model.to(torch.float16) -> Attempting to unscale FP16 gradients.
+from accelerate import notebook_launcher
 
-print('load dataset')
-from datasets import load_dataset
-dataset = load_dataset("pile-of-law/pile-of-law",'r_legaladvice')
-
-def tokenize_function(examples):
-    return tokenizer(examples["text"])
-
-import os
-
-print('preprocess dataset')
-tokenized_dataset = dataset.map(tokenize_function, 
-                                batched=True, 
-                                num_proc=os.cpu_count(),
-                                remove_columns=["text","created_timestamp","downloaded_timestamp","url"],
-)
-
-block_size = 128
-def group_texts(examples):
-    # Concatenate all texts.
-    concatenated_examples = {k: sum(examples[k], []) for k in examples.keys()}
-    total_length = len(concatenated_examples[list(examples.keys())[0]])
-    # We drop the small remainder, we could add padding if the model supported it instead of this drop, you can
-        # customize this part to your needs.
-    total_length = (total_length // block_size) * block_size
-    # Split by chunks of max_len.
-    result = {
- k: [t[i : i + block_size] for i in range(0, total_length, block_size)]
-        for k, t in concatenated_examples.items()
-    }
-    result["labels"] = result["input_ids"].copy()
-    return result
-
-lm_datasets = tokenized_dataset.map(
-    group_texts,
-    batched=True,
-    batch_size=1000,
-    num_proc=os.cpu_count(),
-)
-
-print('creating trainer')
-training_args = TrainingArguments(
-    f"bloom560m-finetuned-pileoflaw_reddit",
-    per_device_train_batch_size=1,
-    gradient_checkpointing=True,
-    gradient_accumulation_steps=1,
-    optim="adafactor",
-    logging_steps=40,
-    save_strategy='epoch',
-    weight_decay=0.1,
-    learning_rate=5e-6,
-    evaluation_strategy='steps',
-    eval_steps=400,
-    per_device_eval_batch_size=1,
-)
-
-trainer = Trainer(
-    model=model,
-    args=training_args,
-    train_dataset=lm_datasets["train"],
-    eval_dataset=lm_datasets["validation"],
-)
-
-trainer.train()
+notebook_launcher(train_single_proc, args=(), num_processes=4)
